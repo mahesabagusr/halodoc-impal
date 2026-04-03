936a4b73529af279c22284799f6336645cd472e0
Mahesa Bagus Raditya <mahesabagusraditya1@gmail.com>
2026-04-03 16:59:48 +0700

    feat: add Swagger documentation for API endpoints
    - Updated package.json to include swagger-jsdoc and swagger-ui-express dependencies.
    - Integrated Swagger UI in the Express app to serve API documentation at /api-docs.
    - Created swagger.ts file to define API documentation structure and schemas.
    - Added type declaration for swagger-jsdoc to support TypeScript.

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,7 +1,9 @@
 import express, { Application, Request, Response } from "express";
+import swaggerUi from "swagger-ui-express";
 import userRoutes from "@/routes/users";
 import pharmacyRoutes from "@/routes/pharmacy";
 import { config } from "@/helpers/infra/global-config";
+import swaggerSpec from "@/docs/swagger";
 
 const app: Application = express();
 const PORT = Number(config.express.port ?? "3000");
@@ -12,6 +14,8 @@ app.use(express.json());
 app.get("/", (req: Request, res: Response) => {
   res.json({ message: "Server is running" });
 });
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.use("/api/v1/users", userRoutes);
 app.use("/api/v1/pharmacy", pharmacyRoutes);
